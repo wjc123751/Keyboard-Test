a9a5ea080100ce26f0b0082a7f6f9e711570bc00
wjc123751 <1600905060@qq.com>
2026-04-17 23:21:57 +0800

    feat: 添加状态栏，过滤编译文件

--- include/keyboardwindow.h
+++ include/keyboardwindow.h
@@ -5,6 +5,7 @@
 #include <QPushButton>
 #include <QTimer>
 #include <QMap>
+#include <QLineEdit>
 
 class KeyboardWindow : public QWidget
 {
@@ -14,13 +15,18 @@ public:
 
 private slots:
     void checkKeyPress();       // 检测按键状态的槽函数
+    void onResetClicked();      // 重置按钮点击事件的槽函数
 
 private:
     void createKeyboard();      // 创建键盘界面
+    void createStatusBar();     // 创建状态栏界面
     
     QMap<int, QPushButton*> m_keyMap;   // 虚拟键码到按钮的映射
     QMap<int, int> m_pressCount;        // 按键按下次数
     QMap<int, bool> m_isLongLight;      // 是否常亮状态
+
+    QLineEdit* vkLabel;         // 显示虚拟键码的标签
+    QLineEdit* scanLabel;       // 显示扫描码的标签
 };
 
 #endif // KEYBOARDWINDOW_H
